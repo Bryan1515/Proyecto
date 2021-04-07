8345e1b885345919aee310346e34b465f54c5171
Bryan1515 <80277294+Bryan1515@users.noreply.github.com>
2021-04-07 11:48:08 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,11 +1,6 @@
-//https://www.eclipse.org/paho/clients/js/
 
-// Create a client instance
-  //client = new Paho.MQTT.Client("postman.cloudmqtt.com", 14970);
-  
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
 
-  // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
   var options = {
@@ -16,14 +11,12 @@
     onFailure:doFail
   }
 
-  // connect the client
+  
   client.connect(options);
    
-  // called when the client connects
+ 
   function onConnect() {
-    // Once a connection has been made, make a subscription and send a message.
     console.log("Conectado...");
-	
     client.subscribe("bryan.loaiza@unach.edu.ec/tema1");
     client.subscribe("bryan.loaiza@unach.edu.ec/tema3");
     message = new Paho.MQTT.Message("hola desde la web");
@@ -32,12 +25,13 @@
 	
   }
 
+
   function doFail(e){
     console.log(e);
 	
   }
 
-  // called when the client loses its connection
+
   function onConnectionLost(responseObject) {
     if (responseObject.errorCode !== 0) {
       console.log("onConnectionLost:"+responseObject.errorMessage);
@@ -46,7 +40,7 @@
 
 
 
-function LED1_On() {
+function senso1() {
 	alert("led on");
 	//console.log("led on");
 	message = new Paho.MQTT.Message("sensor1");
@@ -57,7 +51,7 @@ function LED1_On() {
 
 
 
-function LED1_Off(){	
+function senso2(){	
 	alert("led off");
 	//console.log("led off");
 	message = new Paho.MQTT.Message("sensor2");
